8ab8da4fb8dd198295ef599443dc5b6b7e3bcb55
uzinok <uzinok@yandex.ru>
2020-09-05 14:58:19 +0400

    05.09.2020

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,29 @@
-window.onload = function () {};
+window.onload = function () {
+  let logo = document.querySelector('.logo');
+  let countSquare = 75;
+  let elem;
+  let arrElem = [];
+  let count = 0;
+
+  for (let i = 0; i < countSquare; i++) {
+    elem = document.createElement('div');
+    logo.append(elem);
+
+    arrElem.push(elem);
+  }
+
+  function anim() {
+    arrElem[count].classList.add('anim');
+    count++;
+    if(count > countSquare - 1) return true
+
+    setTimeout(function() {
+      anim();
+    }, 50)
+
+  }
+
+  anim();
+
+  console.log(5 * 15)
+};
